282476ab5603b56cce186ad11eaefe94bef0db30
Muhsin Deniz <muhsin.deniz@arneca.com>
2021-07-30 16:41:35 +0300

    testt

--- src/components.js/Navbar.js
+++ src/components.js/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = (props) => {
           <div class="user-wrapper">
             <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" width="60px" height="60px" alt="" />
             <div>
-              <h6>John Doe</h6>
+              <h6>Selen</h6>
               <small>Super Admin</small>
             </div>
 
